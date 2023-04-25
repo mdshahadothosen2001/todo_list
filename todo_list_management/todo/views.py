aba23c38b221b5add4a6ac3c088e95763e07e111
mdshahadothosen2001 <mdshahadothosen2001@gmail.com>
2023-04-25 18:21:04 +0600

    fea:build template
    1.create task detail page and view task title

--- todo_list_management/todo/views.py
+++ todo_list_management/todo/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from .models import Task
+
+
 class TaskList(ListView):
-    model=Task
+    model = Task
+    context_object_name = 'tasks'
+    template_name = 'todo/task_list.html'
+
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name = 'task'
+    template_name = 'todo/task_detail.html'    
     
+
+        
