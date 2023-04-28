c5a1984c2241efada6309f9e3126341b107bc670
mdshahadothosen2001 <mdshahadothosen2001@gmail.com>
2023-04-28 15:39:07 +0600

    fea:items update view
    1.added new view for items update any item

--- todo_list_management/todo/views.py
+++ todo_list_management/todo/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView , UpdateView
 from django.urls import reverse_lazy
 from .models import Task
 
@@ -9,10 +9,6 @@ from .models import Task
 class TaskList(ListView):
     model = Task
     context_object_name = 'tasks'
-    #django can load by defualt template
-    #but template name should be match class name
-    # like if class name is TaskList
-    #template name must be task_list.html
     template_name = 'todo/task_list.html'
 
 class TaskDetail(DetailView):
@@ -27,5 +23,10 @@ class TaskCreate(CreateView):
     success_url = reverse_lazy('tasks')
     template_name = 'todo/task_form.html' 
 
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
 
         
